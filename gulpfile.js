d03e911fc9efc5d12be3f35c399c09e242cba886
David Castner <davidjcastner@gmail.com>
2016-05-11 13:55:37 -0400

    finish sass gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,6 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var jshint = require('gulp-jshint');
 var browserSync = require('browser-sync').create();
-//var reload      = browserSync.reload;
 
 ////////////////////////////////////////////////////////////////////////////////
 // Directories
@@ -32,41 +31,63 @@ gulp.task('default', ['browser-sync']);
 ////////////////////////////////////////////////////////////////////////////////
 
 gulp.task('jshint-gulpfile', function () {
-    var jsGlob =  "./gulpfile.js";
+    var jsGlob =  './gulpfile.js';
     return gulp.src(jsGlob)
         .pipe(jshint({ 'node': true }))
         .pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('js', function () {
-    var jsGlob =  "./" + DEV_DIR + "/js/*.js";
+    var jsGlob =  './' + DEV_DIR + '/js/*.js';
     return gulp.src(jsGlob)
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'));
 });
 
-// TODO still need to implement the sass watch functionality
 gulp.task('sass', function () {
- return gulp.src('./sass/**/*.scss')
-   .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-   .pipe(gulp.dest('./css'));
+    var sassGlob = './' + DEV_DIR + '/sass/style.scss';
+    var cssDest = './' + DEV_DIR + '/';
+    return gulp.src(sassGlob)
+        .pipe(sass({outputStyle: 'nested'}).on('error', sass.logError))
+        .pipe(gulp.dest(cssDest));
 });
-// end todo
+
+/*
+gulp.task('autoprefixer', ['sass'], function () {
+    var cssGlob = './' + DEV_DIR + '/style.css';
+    return gulp.src(cssGlob)
+        .pipe(autoprefixer({
+            browsers: ['last 3 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest(cssGlob));
+});*/
 
 gulp.task('html-watch', function () {
-    var htmlGlob =  "./" + DEV_DIR + "/*.html";
+    var htmlGlob =  './' + DEV_DIR + '/*.html';
     gulp.watch(htmlGlob).on('change', browserSync.reload);
 });
 
 gulp.task('js-watch', ['js'], function () {
-    var jsGlob =  "./" + DEV_DIR + "/js/*.js";
+    var jsGlob =  './' + DEV_DIR + '/js/*.js';
     gulp.watch(jsGlob).on('change', browserSync.reload);
 });
 
-gulp.task('browser-sync', ['html-watch', 'js-watch'], function() {
+gulp.task('css-watch', function () {
+    var cssGlob = './' + DEV_DIR + '/style.scss';
+    gulp.watch(cssGlob).on('change', browserSync.reload);
+});
+
+gulp.task('sass-watch', function () {
+    var sassGlob = './' + DEV_DIR + '/sass/*.scss';
+    gulp.watch(sassGlob, ['sass']);
+});
+
+gulp.task('browser-sync', ['html-watch', 'js-watch', 'css-watch', 'sass-watch'], function() {
+    var baseDirectory = './' + DEV_DIR + '/';
     browserSync.init({
         server: {
-            baseDir: "./" + DEV_DIR + "/"
+            baseDir: baseDirectory
         }
     });
 });
